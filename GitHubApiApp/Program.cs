450c6e5c13b5ae5154b48e740f98a1b5c1860e07
Beatriz <beatrizlorranedossantos@gmail.com>
2024-09-22 22:22:12 -0300

    Primeiro commit

---
+++ GitHubApiApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://api.github.com/users/mojombo";
+
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+
+            var response = await client.GetStringAsync(url);
+            var json = JObject.Parse(response);
+
+            Console.WriteLine("Nome: " + json["name"]);
+            Console.WriteLine("Empresa: " + json["company"]);
+            Console.WriteLine("Localização: " + json["location"]);
+            Console.WriteLine("Login: " + json["login"]);
+        }
+    }
+}
