70ec659d335379fef2eff19a05039f68282c557d
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2024-09-22 22:31:31 -0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,4 +1,3 @@
-Novidade! Atalhos do teclado … Os atalhos de teclado do Drive foram atualizados para oferecer navegação por letras iniciais
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -7,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace ConsumirApiGitHub
 {
-    // Classe que representa os dados do usuário do GitHub
     public class UsuarioGitHub
     {
         public string Login { get; set; }
@@ -20,35 +18,27 @@ namespace ConsumirApiGitHub
     {
         static async Task Main(string[] args)
         {
-            // URL do endpoint
             string url = "https://api.github.com/user/1";
 
             using (HttpClient client = new HttpClient())
             {
-                // Adicionando User-Agent no cabeçalho da requisição
                 client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("MyApp", "1.0"));
 
                 try
                 {
-                    // Fazendo a requisição GET para o endpoint
                     HttpResponseMessage response = await client.GetAsync(url);
 
-                    // Verificando se a requisição foi bem-sucedida
                     response.EnsureSuccessStatusCode();
 
-                    // Lendo o conteúdo da resposta
                     string responseBody = await response.Content.ReadAsStringAsync();
 
-                    // Configurando opções de desserialização para ignorar maiúsculas e minúsculas
                     var options = new JsonSerializerOptions
                     {
                         PropertyNameCaseInsensitive = true
                     };
 
-                    // Desserializando o JSON para o objeto UsuarioGitHub
                     UsuarioGitHub usuario = JsonSerializer.Deserialize<UsuarioGitHub>(responseBody, options);
 
-                    // Imprimindo os dados solicitados no console
                     Console.WriteLine("Nome: " + usuario.Name);
                     Console.WriteLine("Empresa: " + usuario.Company);
                     Console.WriteLine("Localização: " + usuario.Location);
